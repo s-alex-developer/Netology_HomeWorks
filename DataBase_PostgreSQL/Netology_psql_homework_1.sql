64a36fbcd907b820fb64c3407d39ff8114d738b3
s.alex <s.alex.developer@gmail.com>
2023-04-20 16:42:37 +0300

    new

--- DataBase_PostgreSQL/Netology_psql_homework_1.sql
+++ DataBase_PostgreSQL/Netology_psql_homework_1.sql
@@ -1,24 +1,24 @@
 CREATE TABLE IF NOT EXISTS genres (
 	id SERIAL PRIMARY KEY,
 	name VARCHAR(50) UNIQUE NOT NULL
-	);
+);
 	
 CREATE TABLE IF NOT EXISTS artists (
 	id SERIAL PRIMARY KEY,
 	name VARCHAR(50) UNIQUE NOT NULL
-	);
+);
 
 CREATE TABLE IF NOT EXISTS artists_genres (
 	genre_id INTEGER REFERENCES genres(id) NOT NULL,
 	artist_id INTEGER REFERENCES artists(id) NOT NULL,
 	UNIQUE (genre_id, artist_id)
-	);
+);
 	
 CREATE TABLE IF NOT EXISTS albums (
 	id SERIAL PRIMARY KEY,
 	name VARCHAR(50) NOT NULL,
 	year DATE DEFAULT NULL
-	);
+);
 	
 CREATE TABLE IF NOT EXISTS artists_albums (
 	artist_id INTEGER REFERENCES artists(id) NOT NULL,
