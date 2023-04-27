1ab6295a1e9c5a57e6dc987a54ad130d3fbb9588
s.alex <s.alex.developer@gmail.com>
2023-04-27 21:19:38 +0300

    new

--- DataBase_PostgreSQL/Netology_psql_homework_1_NEW.sql
+++ DataBase_PostgreSQL/Netology_psql_homework_1_NEW.sql
@@ -14,10 +14,10 @@ CREATE TABLE IF NOT EXISTS artists (
 
 CREATE TABLE IF NOT EXISTS artists_genres (
 	 genre_id INTEGER NOT NULL,
-			  FOREIGN KEY (genre_id) REFERENCES genres(id),
+	          FOREIGN KEY (genre_id) REFERENCES genres(id),
 	artist_id INTEGER NOT NULL,
-			  FOREIGN KEY (artist_id) REFERENCES artists(id),
-			   UNIQUE (genre_id, artist_id)			  
+	          FOREIGN KEY (artist_id) REFERENCES artists(id),
+	           UNIQUE (genre_id, artist_id)			  
 );
 	
 CREATE TABLE IF NOT EXISTS albums (
@@ -25,15 +25,15 @@ CREATE TABLE IF NOT EXISTS albums (
 	  id SERIAL,
 	name VARCHAR(50) NOT NULL,
 	year SMALLINT DEFAULT NULL 
-		    CHECK (year BETWEEN 1850 AND 2050) 
+	     CHECK (year BETWEEN 1850 AND 2050) 
 );
 	
 CREATE TABLE IF NOT EXISTS artists_albums (
 	artist_id INTEGER NOT NULL,
-			  FOREIGN KEY (artist_id) REFERENCES artists(id),
+	          FOREIGN KEY (artist_id) REFERENCES artists(id),
 	 album_id INTEGER NOT NULL,
 	          FOREIGN KEY (album_id)  REFERENCES albums(id),
-			   UNIQUE (artist_id, album_id)
+	           UNIQUE (artist_id, album_id)
 );
 
 CREATE TABLE IF NOT EXISTS songs (
@@ -41,9 +41,9 @@ CREATE TABLE IF NOT EXISTS songs (
 	      id SERIAL,
 	    name VARCHAR(50) NOT NULL,
 	album_id INTEGER NOT NULL,
-		     FOREIGN KEY (album_id) REFERENCES albums(id), 
+	         FOREIGN KEY (album_id) REFERENCES albums(id), 
 	duration INTEGER DEFAULT NULL
-			   CHECK (duration BETWEEN 1 AND 48212)
+	         CHECK (duration BETWEEN 1 AND 48212)
 );
 
 CREATE TABLE IF NOT EXISTS collections (
@@ -51,13 +51,13 @@ CREATE TABLE IF NOT EXISTS collections (
 	  id SERIAL,
 	name VARCHAR(50) NOT NULL,
 	year smallint DEFAULT NULL
-		 CHECK(year BETWEEN 1850 AND 2050)
+	     CHECK(year BETWEEN 1850 AND 2050)
 );
 
 CREATE TABLE IF NOT EXISTS collections_songs (
 	collection_id INTEGER NOT NULL,
-				  FOREIGN KEY (collection_id) REFERENCES collections(id),
+	              FOREIGN KEY (collection_id) REFERENCES collections(id),
 	      song_id INTEGER NOT NULL,
-			      FOREIGN KEY (song_id) REFERENCES songs(id),
+	              FOREIGN KEY (song_id) REFERENCES songs(id),
 	               UNIQUE (collection_id, song_id)
 );
