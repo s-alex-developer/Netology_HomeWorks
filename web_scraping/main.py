9c7384dec762b8ec1b9c56008ec003dc145488c2
s.alex <s.alex.developer@gmail.com>
2023-06-09 06:18:47 +0300

    Added changes

--- web_scraping/main.py
+++ web_scraping/main.py
@@ -44,7 +44,7 @@ for p in tqdm(range(9)):
         all_vacancy.append([vacancy_name, vacancy_href, vacancy_offer, vacancy_company, vacancy_town])
 
 all_vacancy.pop(68)
-all_vacancy.pop(72)
+all_vacancy.pop(70)
 
 header = ['position', 'reference', 'offer', 'company', 'town']
 
